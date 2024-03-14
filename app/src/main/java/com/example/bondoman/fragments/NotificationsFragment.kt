a76de63848de7c791454b2fbe4e7fb8df007abee
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2024-03-14 20:06:31 +0700

    refactor: settings

--- app/src/main/java/com/example/bondoman/fragments/NotificationsFragment.kt
+++ app/src/main/java/com/example/bondoman/fragments/NotificationsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.bondoman.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,32 +9,27 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.bondoman.databinding.FragmentNotificationsBinding
+import com.example.bondoman.databinding.FragmentSettingsBinding
 import com.example.bondoman.viewmodels.NotificationsViewModel
 
 class NotificationsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+//        binding.logoutButton.setOnClickListener {
+//            TokenManager.removeToken()
+//            val intent = Intent(activity, LoginActivity::class.java)
+//            startActivity(intent)
+//            activity?.finish()  // finish the current activity (MainActivity)
+//        }
+        return binding.root
     }
 
     override fun onDestroyView() {
