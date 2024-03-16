ef8728e31600aef996a80161193a3e6d7fa738eb
febryanarota <arota0202@gmail.com>
2024-03-16 20:08:13 +0700

    intent email

--- app/src/main/java/com/example/bondoman/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/bondoman/fragments/SettingsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.bondoman.fragments
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.bondoman.activities.LoginActivity
 import com.example.bondoman.databinding.FragmentSettingsBinding
@@ -26,11 +28,30 @@ class SettingsFragment : Fragment() {
             startActivity(intent)
             activity?.finish()  // finish the current activity (MainActivity)
         }
+        val sendButton = binding.sendButton
+        sendButton.setOnClickListener {
+            sendEmail()
+        }
         return binding.root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun sendEmail() {
+        val mailIntent = Intent(Intent.ACTION_SEND)
+        mailIntent.data = Uri.parse("mailto:")
+        mailIntent.type = "text/plain"
+        mailIntent.putExtra(Intent.EXTRA_EMAIL, "13521120@std.stei.itb.ac.id")
+        mailIntent.putExtra(Intent.EXTRA_SUBJECT, "test")
+        mailIntent.putExtra(Intent.EXTRA_TEXT, "lorem ipsum")
+        try {
+            startActivity(Intent.createChooser(mailIntent, "Choose email intent..."))
+        } catch (e: Exception) {
+            Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
+        }
     }
 }
