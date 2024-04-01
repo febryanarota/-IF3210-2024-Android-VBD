038a0ab2dc2d6382da61e70b7fe429ee15c4d157
febryanarota <arota0202@gmail.com>
2024-04-01 08:58:02 +0700

    twibbon done

--- app/src/main/java/com/example/bondoman/fragments/TwibbonFragment.kt
+++ app/src/main/java/com/example/bondoman/fragments/TwibbonFragment.kt
@@ -1,28 +1,20 @@
 package com.example.bondoman.fragments
 
 import android.Manifest
-import android.graphics.Bitmap
-import android.graphics.ImageDecoder
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageProxy
 import androidx.camera.view.LifecycleCameraController
 import androidx.lifecycle.lifecycleScope
-import com.example.bondoman.R
 import com.example.bondoman.databinding.FragmentTwibbonBinding
 import com.example.bondoman.utils.PermissionUtils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.io.File
 import java.util.concurrent.Executors
 
 class TwibbonFragment : Fragment() {
@@ -55,18 +47,28 @@ class TwibbonFragment : Fragment() {
         binding.captureButton.setOnClickListener {
             onCaptureButtonClicked()
         }
-//        binding.retakeButton.setOnClickListener {
-//            onRetakeButtonClicked()
-//        }
+        binding.retakeButton.setOnClickListener {
+            onRetakeButtonClicked()
+        }
+
+        binding.flipButton.setOnClickListener {
+            if (camera.cameraSelector == CameraSelector.DEFAULT_FRONT_CAMERA) {
+                camera.cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            } else {
+                camera.cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+            }
+        }
     }
 
     private fun onCaptureButtonClicked() {
+        binding.captureButton.visibility = View.GONE
+        binding.retakeButton.visibility = View.VISIBLE
+        binding.flipButton.setEnabled(false)
         val cameraExecutor = Executors.newSingleThreadExecutor()
         camera.takePicture(
             cameraExecutor,
             object: ImageCapture.OnImageCapturedCallback() {
                 override fun onCaptureSuccess(image: ImageProxy) {
-//                    playShutterSound()
                     lifecycleScope.launch(Dispatchers.Main) {
                         binding.previewView.controller = null
                     }
@@ -75,19 +77,13 @@ class TwibbonFragment : Fragment() {
         )
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TwibbonFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TwibbonFragment().apply {
-            }
+    private fun onRetakeButtonClicked() {
+        binding.captureButton.visibility = View.VISIBLE
+        binding.retakeButton.visibility = View.GONE
+        binding.flipButton.setEnabled(true)
+        camera = LifecycleCameraController(requireContext())
+        camera.bindToLifecycle(this)
+        camera.cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+        binding.previewView.controller = camera
     }
 }
